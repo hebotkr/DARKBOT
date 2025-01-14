51086f1d36bdfe8d233a2dcac3137903055cd234
Project-kVD <projectnewupdate@gmail.com>
2025-01-14 20:52:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GqZmHb7D#OXwB5jSWSH_V8S7FprsgopTqwtdA3y0dzqaxGla_Sxc ",
+SESSION_ID: process.env.SESSION_ID || "GqZmHb7D#OXwB5jSWSH_V8S7FprsgopTqwtdA3y0dzqaxGla_Sxc",
 MONGODB: process.env.MONGODB || "mongodb://mongo:EPFIYNcKWdSVoMSaZGhcnFNoKnTryKFY@junction.proxy.rlwy.net:34409",
 };
