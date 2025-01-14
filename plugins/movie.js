cdefdac7de9af7bdfbf801b3f72a45f93342adf8
Project-kVD <projectnewupdate@gmail.com>
2025-01-14 22:20:47 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -19,11 +19,12 @@ cmd({
     category: "download",
     desc: "Search movies on sinhalasub and get download links",
     filename: __filename
-}, async (conn, m, mek, { from, isMe, isOwner, q, reply }) => {
-    try {
+    async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumner, botNumner2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try{
+    
         // Check if search query is provided
         if (!q || q.trim() === '') return await reply('*Please provide a search query! (e.g., Deadpool)*');
-        if (!isMe && !isOwner) return await reply('*Only Bot Number Can Movie Download !!!*');
+
 
         // Fetch search results from API
         const manu = await fetchJson(`${domain}/api/sl-sub-search?query=${q}&apikey=${api_key}`);
