5f66c6a0eb98ab712c360316b2865bd3696e245c
Project-kVD <projectnewupdate@gmail.com>
2025-01-16 16:24:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "P6BDARZS#vfFycq3jTYboTc3CPORt_LRZ5qKu--CeOpw6qshdnf0",
+SESSION_ID: process.env.SESSION_ID || "P6BDARZS#vfFycq3jTYboTc3CPORt_LRZ5qKu--CeOpw6qshdnf0 ",
 MONGODB: process.env.MONGODB || "mongodb://mongo:EPFIYNcKWdSVoMSaZGhcnFNoKnTryKFY@junction.proxy.rlwy.net:34409",
 };
