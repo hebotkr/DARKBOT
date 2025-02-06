19fd637dc888565ac8d4a48618d886d3cb9e5663
hebotkr <heprabathmdacc@gmail.com>
2025-02-06 22:37:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ const port = process.env.PORT || 8000;
 
 async function connectToWA() {
 //==============connect mongodb=============
-const connectDB = require('./lib/mongodb')
-connectDB();
+
 //==============================
 const {readEnv} = require('./lib/database')
 const config = await readEnv();
